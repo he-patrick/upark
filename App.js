d70c16b4b1c227bbe0dde43549362296c85fa6ea
Patrick He <77468352+he-patrick@users.noreply.github.com>
2025-01-18 16:11:28 -0500

    Add routing

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import MapView, { Marker } from 'react-native-maps';
 import BottomNavBar from './components/BottomNavBar';
 import SplashScreen from './components/SplashScreen';
 import * as Location from 'expo-location';
-import MapsPage from './components/MapsPage';
+import LoginPage from './components/LoginPage';
+import SelectUserPage from './components/SelectUserPage';
+import WelcomePage from './components/WelcomePage';
+import PaymentPage from './components/PaymentPage';
 import DashboardPage from './components/DashboardPage';
+import MapsPage from './components/MapsPage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -36,11 +40,15 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator 
-        initialRouteName="Home"
+        initialRouteName="Login"
         screenOptions={{
           headerShown: false // This hides the navigation header
         }}
       >
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="SelectUser" component={SelectUserPage} />
+        <Stack.Screen name="Welcome" component={WelcomePage} />
+        <Stack.Screen name="Payment" component={PaymentPage} />
         <Stack.Screen name="DashboardPage" component={DashboardPage} />
         <Stack.Screen name="Maps" component={MapsPage} />
       </Stack.Navigator>
