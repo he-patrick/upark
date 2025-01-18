e6aad96c132d60c13c7c4629589febe5ebab44f5
Sultreedolfin <karenwang876@gmail.com>
2025-01-18 15:05:40 -0500

    added fade-out animation on login

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
   View,
   Image,
   StyleSheet,
   Dimensions,
+  Animated,
 } from 'react-native';
 
 const SplashScreen = () => {
+  const fadeAnim = useRef(new Animated.Value(1)).current; // Initial opacity 1
+
+  useEffect(() => {
+    // Start the fade-out animation after the component mounts
+    Animated.timing(fadeAnim, {
+      toValue: 0, // Fade out to opacity 0
+      duration: 2000, // Duration of 2 seconds
+      useNativeDriver: true, // Optimize for performance
+    }).start();
+  }, []);
   return (
     <View style={styles.splashScreen}>
-      <Image 
-        source={require('../assets/logo.png')}
-        style={styles.logo}
-        resizeMode="contain"
-      />
+      <Animated.View style={{ opacity: fadeAnim }}>
+        <Image 
+          source={require('../assets/logo.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </Animated.View>
     </View>
+    
   );
 };
 
